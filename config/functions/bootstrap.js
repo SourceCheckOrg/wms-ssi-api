5b030b09e706d03800b37d27867612f673b5c53a
Jefferson Sofarelli <jmsofarelli@protonmail.com>
2021-02-17 10:29:45 +0000

    Setup app config using env variables. Setup socket.io server and connection with redis db.

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,4 +1,5 @@
-'use strict';
+"use strict";
+const redis = require("redis");
 
 /**
  * An asynchronous bootstrap function that runs before
@@ -10,4 +11,49 @@
  * See more details here: https://strapi.io/documentation/developer-docs/latest/concepts/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+module.exports = async () => {
+  process.nextTick(() => {
+    // Setup redis client and register it in strapi main object to use it globally
+    console.log("Setup redis");
+    const redisHost = strapi.config.get("redis.host");
+    const redisPort = strapi.config.get("redis.port");
+    console.log("Redis url:", `${redisHost}:${redisPort}`);
+    strapi.redis = redis.createClient({
+      host: strapi.config.get("redis.host"),
+      port: strapi.config.get("redis.port"),
+    });
+
+    // Setup socket.io server
+    console.log("Setup socket.io");
+    const frontendHost = strapi.config.get("frontend.host");
+    const frontendPort = strapi.config.get("frontend.port");
+    console.log("Frontend url:", `${frontendHost}:${frontendPort}`);
+
+    var io = require("socket.io")(strapi.server, {
+      cors: {
+        origin: `${frontendHost}:${frontendPort}`,
+        methods: ["GET", "POST"],
+      },
+    });
+    io.on("connection", async function (socket) {
+      console.log(`User connected - socket: `, socket.id);
+
+      // Send message on user connection
+      socket.emit("hello", JSON.stringify({ message: "user connected" }));
+
+      // Listen for user diconnect
+      socket.on("disconnect", () => {
+        console.log("User disconnected");
+      });
+
+      // Associate a socket with a client token
+      socket.on("client-token-sub", (token) => {
+        console.log("client-token-sub", token);
+        strapi.redis.set(token, socket.id);
+      });
+    });
+
+    // Register socket.io inside strapi main object to use it globally
+    strapi.io = io;
+  });
+};
--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('URL', 'http://localhost:1337'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '0f4a79c8f889a636aa8ff37bb9bcfca8'),
