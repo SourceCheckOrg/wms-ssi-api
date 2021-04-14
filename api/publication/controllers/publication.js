c0f1474a20ef54c2b0760e53a9f1a9b2d3c2e77d
Jefferson Sofarelli <jmsofarelli@protonmail.com>
2021-04-14 21:52:20 +0100

    When user uploads a new pdf file to a publication, delete the old one. When the user deletes a publication containing a pdf file, delete it as well.

--- api/publication/controllers/publication.js
+++ api/publication/controllers/publication.js
@@ -33,9 +33,23 @@ module.exports = {
     // Update publication
     let entity;
     if (ctx.is('multipart')) {
+      // User is uploading a pdf file. Delete the old one if it exists
+      if (publication.pdf_raw) {
+        const file = await strapi.plugins['upload'].services.upload.fetch({ id: publication.pdf_raw.id });
+        await strapi.plugins['upload'].services.upload.remove(file);
+      }
+
+      // Save publication
       const { data, files } = parseMultipartData(ctx);
       entity = await strapi.services.publication.update({ id }, data, { files });
     } else {
+      // If user is deleting the pdf file, pdf_raw is explicitly set to null (type 'object'), otherwise pdf_raw is not set in the request (type 'undefined')
+      if (typeof ctx.request.body.pdf_raw === 'object') {
+        const file = await strapi.plugins['upload'].services.upload.fetch({ id: publication.pdf_raw.id });
+        await strapi.plugins['upload'].services.upload.remove(file);
+      }
+
+      // Save publication
       entity = await strapi.services.publication.update({ id }, ctx.request.body );
     }
 
@@ -51,6 +65,12 @@ module.exports = {
     // Check if the user is the owner of the publication
     const publication = await strapi.services.publication.findOne({ id, 'owner.id': ctx.state.user.id });
     if (!publication) return ctx.unauthorized("You can't delete this publication!");
+
+    // If the publication contains a pdf file attached, delete it as well
+    if (publication.pdf_raw) {
+      const file = await strapi.plugins['upload'].services.upload.fetch({ id: publication.pdf_raw.id });
+      await strapi.plugins['upload'].services.upload.remove(file);
+    }
 
     // Delete publication
     await strapi.services.publication.delete({ id });
